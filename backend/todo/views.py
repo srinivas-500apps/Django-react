dfb0e4f5b9b2a1de6e7d8bf1ed7af80a8a737930
srinivaschikkala <srinivaschikkala2112gmail.com>
2024-02-18 14:28:30 +0530

    Todo

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -8,9 +8,3 @@ from .models import Todo
 class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def start_project(request):
-    """Start project"""
-    return HttpResponse("you are successfully started jango project")
