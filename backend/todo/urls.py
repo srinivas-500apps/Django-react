6052c2d577ae661b13d580afa80fc98a430ae475
srinivaschikkala <srinivaschikkala2112gmail.com>
2024-02-18 10:54:17 +0530

    changes

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+
+from django.urls import path, include
+from rest_framework import routers
+from todo import views
+
+
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoView, 'todo')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('mypath/',include('todo.urls'))
+    path('start_project', views.start_project, name="todos"),
 ]
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
 
 # Create your views here.
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def start_project(request):
+    """Start project"""
+    return HttpResponse("you are successfully started jango project")
